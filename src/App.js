590209c55741fb6b6465daecf95bd14d569f1a48
MUHAMMED YASEEN N A <121yaseen@gmail.com>
2021-01-06 08:56:58 +0530

    Basic Routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Layout from "./hoc/Layout/Layout";
+import Checkout from "../src/containers/Checkout/Checkout";
 
 class App extends Component {
-	render() {
-		return (
-			<div>
-				<Layout>
-					<BurgerBuilder />
-				</Layout>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" component={BurgerBuilder} />
+          </Switch>
+          {/* <BurgerBuilder /> */}
+          {/* <Checkout /> */}
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
